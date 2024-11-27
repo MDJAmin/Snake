b66ab4bd38b3f7aff5db351b560e022d79567bc9
MDJAmin <mohammadamindf@gmail.com>
2024-11-27 21:02:33 +0330

    Feat : defined all variables

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@ const ctx = canvas.getContext("2d"); // type of our dimensional (its 2d in our c
 const btnStart = document.querySelector(".btn-start");
 const btnPause = document.querySelector(".btn-pause");
 const scoreVal = document.querySelector(".score-val");
+
+let direction;
+const DIR = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+};
+
+// ctx.strokeStyle = '#616161'
+ctx.strokeStyle = "#27373F";
+ctx.fillStyle = "#fff";
+
+let snake = [];
+let food = null;
+let paused = false;
+let needsGrowth = false;
+
+let lastUpdate, lastFood, tick;
+let state;
+let flash = false;
+let lastKeyPressed;
